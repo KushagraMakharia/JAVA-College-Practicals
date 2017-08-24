caadf7cdfdd229df1a725be0b89e3214f71a5daf
Kushagra Makharia <KushagraMakharia@users.noreply.github.com>
2017-08-24 14:16:19 +0530

    Add files via upload

---
+++ Object.java
@@ -0,0 +1,10 @@
+public class Object {
+   public void Puppy(String name) {
+   System.out.println("Name is :" + name );
+   }
+
+   public static void main(String []args) {
+   Object myPuppy = new Object();
+   myPuppy.Puppy("Hellio");
+   }
+}
