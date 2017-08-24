83343bdaf2d16c86ae795e5e931abcc603e78f83
Kushagra Makharia <KushagraMakharia@users.noreply.github.com>
2017-08-24 13:16:36 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,16 @@
+class Prime{
+	public static void main(String[] args){
+		int n = 7;
+		int flag=0;
+		for(int i = 2; i <= n/2;i++){
+			if(n%i == 0){
+				flag = 1;
+			}
+		}
+		if(flag == 1){
+			System.out.println("Not Prime!");
+			} else {
+				System.out.println("Prime!");
+				}
+	}
+}
